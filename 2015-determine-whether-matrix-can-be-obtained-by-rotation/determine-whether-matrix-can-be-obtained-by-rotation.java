574a4ebaf5c7bb35275cb5e361f7e1594bb0ec45
Mohamed Ali Raihan <97342788+rey-arx@users.noreply.github.com>
2026-02-15 15:01:34 +0530

    Time: 1 ms (42.74%) | Memory: 43.5 MB (76.70%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,62 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        boolean res = false;
+        //looping 4 times to check all 360 degree
+        for(int i=0;i<4;i++){
+            //finding the transpose:
+            for(int j=0;j<mat.length;j++){
+                for(int k=0;k<mat.length;k++){
+                    if(k>j){
+                        int temp = mat[j][k];
+                        mat[j][k] = mat[k][j];
+                        mat[k][j] = temp;
+                    }
+                }
+            }
+            // System.out.println( " trans mat" + i );
+            // for(int j=0;j<mat.length;j++){
+            //     for(int k=0;k<mat.length;k++){
+            //         System.out.print(mat[j][k]+ " " );
+                    
+            //     }
+            //     System.out.println();
+            // }
+            // reversing the array
+            for(int l=0;l<mat.length;l++){
+            int j =0;
+            int k = mat.length-1;
+            while(j<k){
+                int temp = mat[l][k];
+                mat[l][k] = mat[l][j];
+                mat[l][j] = temp;
+                j++;
+                k--;
+            }
+            }
+            // System.out.println( "rev mat" + i );
+            // for(int j=0;j<mat.length;j++){
+            //     for(int k=0;k<mat.length;k++){
+            //         System.out.print(mat[j][k]+ " " );
+                    
+            //     }
+            //     System.out.println();
+            // }
+            // checking if equal
+            boolean res1=true;
+            for(int j=0;j<mat.length;j++){
+                for(int k=0;k<mat.length;k++){
+                    if(mat[j][k] != target[j][k]){
+                        res1 = false;
+                        break;
+                    }
+                }
+                if(res1 == false) break;
+            }
+           if(res1 == true){
+            res = true;
+            break; 
+           } 
+        }
+        return res;
+    }
+}
